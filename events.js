ef9e469ad82aab302e4cb6b688bbe4533ea32285
shohankazi <shohankazi012@gmail.com>
2023-05-02 20:58:33 +0600

    code added

--- events.js
+++ events.js
@@ -1,19 +1,31 @@
 // Option 1: directly set on the HTML element
 
+// <button onclick="console.log('I am clicked!');">Another button </button>
 
 // Option 2 : Add onclick function
 function makeRed() {
-    document.body.style.backgroundColor = 'red';
+  document.body.style.backgroundColor = "red";
 }
 // Option 3 :
-const makeBlueButton = document.getElementById('make-blue');
+const makeBlueButton = document.getElementById("make-blue");
 makeBlueButton.onclick = makeBlue;
 function makeBlue() {
-    document.body.style.backgroundColor = 'blue';
+  document.body.style.backgroundColor = "blue";
 }
 // Option 3 another :
-const purpleButton = document.getElementById('make-purple');
-purpleButton.onclick = function makePurple()
-    {
-        document.body.style.backgroundColor = 'purple';
-    }
+const purpleButton = document.getElementById("make-purple");
+purpleButton.onclick = function makePurple() {
+  document.body.style.backgroundColor = "purple";
+};
+
+// Option 4
+const pinkButton = document.getElementById("make-pink");
+pinkButton.addEventListener("click", function () {
+  document.body.style.backgroundColor = "pink";
+});
+// Option 4 another 
+const makeGreenButton = document.getElementById('make-green');
+    makeGreenButton.addEventListener('click',function(){
+        document.body.style.backgroundColor = 'green';
+    })
+// Option 4 Final
