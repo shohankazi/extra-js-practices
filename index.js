fe490ce2fefa3f7a3f990d8805cd428523015440
shohankazi <shohankazi012@gmail.com>
2023-05-04 09:17:33 +0600

    comment box added

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-// const list = document.getElementById('list');
-// console.log(list);
-// const ulList = document.createElement('li');
-// ulList.innerText = 'list no - 6';
-// list.appendChild(ulList);
+const list = document.getElementById('list');
+console.log(list);
+const ulList = document.createElement('li');
+ulList.innerText = 'list no - 6';
+list.appendChild(ulList);
 
-// document.addEventListener('click',function(){
-//     const list = document.getElementById('list');
-//     const listItem = document.createElement('li');
-//     listItem.innerText = 'list item added';
-//     list.appendChild(listItem);
-// })
+document.addEventListener('click',function(){
+    const list = document.getElementById('list');
+    const listItem = document.createElement('li');
+    listItem.innerText = 'list item added';
+    list.appendChild(listItem);
+})
 
