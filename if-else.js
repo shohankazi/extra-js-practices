f9784eb290eaa541f626d2bbcf52ac466a29b076
shohankazi <shohankazi012@gmail.com>
2023-05-14 11:33:11 +0600

    GPA Calculator made by if else condition

---
+++ if-else.js
@@ -0,0 +1,24 @@
+// Calculate GPA
+
+var result = 70;
+if (result >= 80) {
+    console.log("You have passed! You got A+");
+}
+else if(result >=70){
+    console.log("You have passed! You got A");
+}
+else if(result >= 60){
+    console.log("You have passed! You got A-");
+}
+else if(result >= 50){
+    console.log("You have passed! You got B");
+}
+else if(result >= 40){
+    console.log("You have passed! You got C");
+}
+else if(result>= 33){
+    console.log("You have passed! You got D");
+}
+else {
+    console.log("Sorry! You are failed. Go and study well");
+}
