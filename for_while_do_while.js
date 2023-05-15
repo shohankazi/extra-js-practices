c3816fdf6e5194957bde3af2e145a70998adbc5d
shohankazi <shohankazi012@gmail.com>
2023-05-15 11:25:47 +0600

    code added

---
+++ for_while_do_while.js
@@ -0,0 +1,57 @@
+// ------------- show numbers from 1 to 5 using for loop ---------
+
+/*
+for(i = 1; i <= 5; i++){
+    console.log(i,'\n');
+}
+*/
+
+// -------------- show numbers from 1 to 5 using while loop ----------
+
+/*
+var i = 1;
+while (i<=5) {
+    console.log(i);
+    i++;
+}
+*/
+
+// ------------------ show numbers from 1 to 5 using do while loop ------------
+
+/*
+var i = 1;
+do {
+    console.log(i);
+    i++;
+} while (i <= 5);
+*/
+
+// -------------------- Show odd numbers using for loop ----------------
+
+/*
+for(i = 1; i <= 10; i++){
+    if (i % 2 === 1) {
+        console.log(i);
+    }
+}
+*/
+
+//  -------------------------- Show odd numbers using while loop ----------------
+
+/*
+var i = 1
+while (i <= 10) {
+    console.log(i);
+    i = i + 2;
+}
+*/
+
+// -------------------- Show odd numbers using do while loop ---------------
+
+/*
+var i = 1;
+do {
+    console.log(i);
+    i += 2;
+} while (i<=10);
+*/
